dae25a1069fcf26b1990f40a2475a36267c5a71f
Tue Do <tuedo2@illinois.edu>
2025-01-09 02:05:11 -0600

    TransformerBlock

--- models.py
+++ models.py
@@ -28,50 +28,24 @@ def create_posistional_embedding(max_len, n_embd):
 
 class TransformerBlock(nn.Module):
     """
-    Transformer block with self-attention and triangle masking
+    Implementation inspired from https://web.stanford.edu/~jurafsky/slp3/9.pdf
     """
-
-    def __init__(self, n_embd=128, n_head=4):
-        super(TransformerBlock, self).__init__()
-
-        # Layer normalization for input
+    def __init__(self, n_embd=512, n_heads=4, dropout=0.1):
         self.norm1 = nn.LayerNorm(n_embd)
-
-        # Multi-head self-attention
-        self.multihead_attn = nn.MultiheadAttention(n_embd, num_heads=n_head, dropout=0.2, batch_first=True)
-
-        # Layer normalization for attention output
+        self.multiheadattention = nn.MultiheadAttention(n_embd, n_heads, dropout=dropout)
         self.norm2 = nn.LayerNorm(n_embd)
-
-        # Feed-forward neural network
-        self.mlp = nn.Sequential(
-            nn.Linear(n_embd, n_embd * 4),
+        self.feedforward = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
             nn.ReLU(),
-            nn.Linear(n_embd * 4, n_embd)
+            nn.Linear(4 * n_embd, n_embd)
         )
-
-    def forward(self, x, padding_mask=None):
-        b_size, layers, heads = x.shape
-        mask = torch.triu(torch.ones(layers, layers, device=x.device), 1).bool()
-
-        norm_x = self.norm1(x)
-        x = self.multihead_attn(norm_x, norm_x, norm_x, attn_mask=mask, key_padding_mask=padding_mask)[0] + x
-        
-        norm_x = self.norm2(x)
-        x = self.mlp(norm_x) + x
-
-        return x
     
-class TransformerModel(nn.Module):
-    """
-    Decoder-only Transformer with self-attention
-    """
+    def forward(self, x):
+        t1 = self.norm1(x)
+        t2 = self.multiheadattention(x, x, x, causal_mask = True)
+        t3 = t1 + t2
+        t4 = self.norm2(t3)
+        t5 = self.feedforward(t4)
+        h = t5 + t3
 
-    def __init__(self, n_positions=128, n_embd=128, n_layer=4, n_head=4):
-        super(TransformerModel, self).__init__()
-        self.pos_emb = create_posistional_embedding(n_positions, n_embd)
-        self.blocks = nn.ModuleList([
-            TransformerBlock(n_embd, n_head) for _ in range(n_layer)
-        ])
-
-        self.out = nn.Linear(n_embd)
+        return h
